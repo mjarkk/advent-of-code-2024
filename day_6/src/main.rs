840465f8142cadfbfd2ce10c462a82f77316ebe0
mjarkk <mkopenga@gmail.com>
2024-12-07 13:46:04 +0100

    Reduce steps to walk out of map

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -19,7 +19,7 @@ fn main() {
 
     let mut location_visit_flags: Vec<u8> = Vec::with_capacity(puzzle.len());
 
-    let mut walls_lookups: Vec<[u8; 4]> = Vec::with_capacity(puzzle.len());
+    let mut walls_lookups: Vec<[usize; 4]> = Vec::with_capacity(puzzle.len());
 
     for (line_idx, line) in puzzle.lines().enumerate() {
         puzzle_size.width = line.len();
@@ -45,60 +45,49 @@ fn main() {
 
     for x in 0..puzzle_size.width {
         // From top bottom
-        let mut last_wall: Option<usize> = None;
+        let mut last_wall: isize = -1;
         for y in 0..puzzle_size.height {
             let idx = y * puzzle_size.width + x;
             if walls[idx] {
-                last_wall = Some(y);
-                continue;
-            }
-
-            if let Some(last_wall) = last_wall {
-                walls_lookups[idx][DIRECTION_UP as usize] = (y - last_wall - 1) as u8;
+                last_wall = y as isize;
+            } else {
+                walls_lookups[idx][DIRECTION_UP as usize] = (y as isize - last_wall - 1) as usize;
             }
         }
 
         // From bottom to top
-        last_wall = None;
+        last_wall = puzzle_size.height as isize;
         for y in (0..puzzle_size.height).rev() {
             let idx = y * puzzle_size.width + x;
             if walls[idx] {
-                last_wall = Some(y);
-                continue;
-            }
-
-            if let Some(last_wall) = last_wall {
-                walls_lookups[idx][DIRECTION_DOWN as usize] = (last_wall - y - 1) as u8;
+                last_wall = y as isize;
+            } else {
+                walls_lookups[idx][DIRECTION_DOWN as usize] = (last_wall - y as isize - 1) as usize;
             }
         }
     }
 
     for y in 0..puzzle_size.height {
         // From left to right
-        let mut last_wall: Option<usize> = None;
+        let mut last_wall: isize = -1;
         for x in 0..puzzle_size.width {
             let idx = y * puzzle_size.width + x;
             if walls[idx] {
-                last_wall = Some(x);
-                continue;
-            }
-
-            if let Some(last_wall) = last_wall {
-                walls_lookups[idx][DIRECTION_LEFT as usize] = (x - last_wall - 1) as u8;
+                last_wall = x as isize;
+            } else {
+                walls_lookups[idx][DIRECTION_LEFT as usize] = (x as isize - last_wall - 1) as usize;
             }
         }
 
         // From right to left
-        last_wall = None;
+        last_wall = puzzle_size.width as isize;
         for x in (0..puzzle_size.width).rev() {
             let idx = y * puzzle_size.width + x;
             if walls[idx] {
-                last_wall = Some(x);
-                continue;
-            }
-
-            if let Some(last_wall) = last_wall {
-                walls_lookups[idx][DIRECTION_RIGHT as usize] = (last_wall - x - 1) as u8;
+                last_wall = x as isize;
+            } else {
+                walls_lookups[idx][DIRECTION_RIGHT as usize] =
+                    (last_wall - x as isize - 1) as usize;
             }
         }
     }
@@ -228,7 +217,7 @@ fn resolve_p2(
     mut player_location: usize,
     puzzle_size: &PuzzleSize,
     location_visit_flags: &mut Vec<u8>,
-    walls_lookups: &Vec<[u8; 4]>,
+    walls_lookups: &Vec<[usize; 4]>,
     walls_lookup_skip_axis: usize,
 ) -> bool {
     let directions: [u8; 4] = [
@@ -250,9 +239,34 @@ fn resolve_p2(
     let walls_lookup_skip_axis = (skip_x, skip_y);
 
     loop {
-        let y = player_location / puzzle_size.width;
-        let x = player_location % puzzle_size.width;
-        let mut new_player_location = match direction {
+        let mut y = player_location / puzzle_size.width;
+        let mut x = player_location % puzzle_size.width;
+
+        let (skip_x, skip_y) = walls_lookup_skip_axis;
+        if skip_y != y && skip_x != x {
+            let next_walls = walls_lookups[player_location][direction as usize];
+            if next_walls > 0 {
+                player_location = match direction {
+                    DIRECTION_UP => player_location - (puzzle_size.width * next_walls),
+                    DIRECTION_LEFT => player_location - next_walls as usize,
+                    DIRECTION_DOWN => player_location + (puzzle_size.width * next_walls),
+                    DIRECTION_RIGHT => player_location + next_walls as usize,
+                    _ => player_location,
+                };
+
+                let flags = location_visit_flags[player_location];
+                let direction_flag = 1 << direction;
+                if flags == flags | direction_flag {
+                    return false;
+                }
+                location_visit_flags[player_location] = flags | direction_flag;
+
+                y = player_location / puzzle_size.width;
+                x = player_location % puzzle_size.width;
+            }
+        }
+
+        let new_player_location = match direction {
             DIRECTION_UP => {
                 if y == 0 {
                     break;
@@ -286,22 +300,6 @@ fn resolve_p2(
             direction_idx = (direction_idx + 1) % 4;
             direction = directions[direction_idx];
             continue;
-        }
-
-        let (skip_x, skip_y) = walls_lookup_skip_axis;
-        if skip_y != y && skip_x != x {
-            let next_walls = walls_lookups[new_player_location][direction as usize];
-            if next_walls > 0 {
-                new_player_location = match direction {
-                    DIRECTION_UP => new_player_location - (puzzle_size.width * next_walls as usize),
-                    DIRECTION_LEFT => new_player_location - next_walls as usize,
-                    DIRECTION_DOWN => {
-                        new_player_location + (puzzle_size.width * next_walls as usize)
-                    }
-                    DIRECTION_RIGHT => new_player_location + next_walls as usize,
-                    _ => new_player_location,
-                };
-            }
         }
 
         player_location = new_player_location;
