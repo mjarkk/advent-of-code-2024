96123d0a8dee897cff1fff636ec149cf2a5c892f
mjarkk <mkopenga@gmail.com>
2024-12-08 19:12:59 +0100

    Solve day 7 in reverse making it at least 20x faster compared to the previouse single core version

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,51 +1,14 @@
 use std::fs;
-use std::sync::mpsc::channel;
-use std::thread;
 use std::time::Instant;
 
 fn main() {
     let now = Instant::now();
     let puzzle = fs::read_to_string("./puzzle.txt").unwrap();
 
-    let paralism = thread::available_parallelism().unwrap().get();
-    let mut chunks: Vec<Vec<String>> = Vec::with_capacity(paralism);
-    for _ in 0..paralism {
-        chunks.push(Vec::new());
-    }
-    for (idx, line) in puzzle.lines().enumerate() {
-        chunks[idx % paralism].push(line.to_string());
-    }
-
-    let (sender, receiver) = channel::<(usize, usize)>();
-
-    for chunk in chunks {
-        let sender_clone = sender.clone();
-        thread::spawn(move || {
-            let results = solve_chunk(chunk);
-            let _ = sender_clone.send(results);
-        });
-    }
-    drop(sender);
-
     let mut result_p1 = 0usize;
     let mut result_p2 = 0usize;
 
-    while let Ok((p1, p2)) = receiver.recv() {
-        result_p1 += p1;
-        result_p2 += p2;
-    }
-
-    println!("p1 {}", result_p1);
-    println!("p2 {}", result_p1 + result_p2);
-
-    println!("Elapsed: {:.2?}", now.elapsed());
-}
-
-fn solve_chunk(chunk: Vec<String>) -> (usize, usize) {
-    let mut result_p1 = 0usize;
-    let mut result_p2 = 0usize;
-
-    for line in chunk {
+    for line in puzzle.lines() {
         if line.is_empty() {
             continue;
         }
@@ -69,7 +32,10 @@ fn solve_chunk(chunk: Vec<String>) -> (usize, usize) {
         }
     }
 
-    (result_p1, result_p2)
+    println!("p1 {}", result_p1);
+    println!("p2 {}", result_p1 + result_p2);
+
+    println!("Elapsed: {:.2?}", now.elapsed());
 }
 
 enum Result {
@@ -85,14 +51,22 @@ struct Solver {
 
 impl Solver {
     fn solve(expected: usize, numbers: Vec<usize>) -> Result {
-        let first_nr = numbers[0];
+        // let first_nr = numbers[0];
         let solver = Self { numbers, expected };
 
         if let Some(result) = solver.check_nothing_to_solve() {
             return result;
         }
 
-        solver.solve_recursive(first_nr, 1)
+        match solver.solve_recursive_reverse(expected, solver.numbers.len() - 1, false) {
+            Result::Solved => return Result::Solved,
+            Result::SolvedWithCombinator => panic!("wut? this should not happen"),
+            Result::NotSolved => {
+                // try the same with the combine operator
+            }
+        };
+
+        solver.solve_recursive_reverse(expected, solver.numbers.len() - 1, true)
     }
     fn check_nothing_to_solve(&self) -> Option<Result> {
         if self.numbers.len() != 1 {
@@ -103,47 +77,44 @@ impl Solver {
             Some(Result::NotSolved)
         }
     }
-    fn solve_recursive(&self, current: usize, offset: usize) -> Result {
-        let next = self.numbers[offset];
-        let is_last_element = offset == self.numbers.len() - 1;
+    fn solve_recursive_reverse(
+        &self,
+        current_answer: usize,
+        offset: usize,
+        use_combine_operator: bool,
+    ) -> Result {
+        let nr = self.numbers[offset];
 
-        for operator in 0..3 {
-            let result = match operator {
-                0 => current + next,
-                1 => current * next,
-                2 => {
-                    let mut exp = 1u32;
-                    let mut temp = next;
-                    loop {
-                        if temp < 10 {
-                            break;
-                        }
+        if offset == 0 {
+            return if current_answer == nr {
+                Result::Solved
+            } else {
+                Result::NotSolved
+            };
+        }
 
-                        temp = temp / 10;
-                        exp += 1;
+        for operator in 0..(if use_combine_operator { 3 } else { 2 }) {
+            let new_answer = match operator {
+                0 => {
+                    if nr > current_answer {
+                        continue;
                     }
-
-                    current * usize::pow(10, exp) + next
+                    current_answer - nr
                 }
+                1 => {
+                    if nr > current_answer || current_answer % nr != 0 {
+                        continue;
+                    }
+                    current_answer / nr
+                }
+                2 => match cut_number_from_end(nr, current_answer) {
+                    Some(answer) => answer,
+                    None => continue,
+                },
                 _ => panic!("Unknown operator"),
             };
 
-            if result > self.expected {
-                continue;
-            }
-
-            if is_last_element {
-                if result == self.expected {
-                    return if operator == 2 {
-                        Result::SolvedWithCombinator
-                    } else {
-                        Result::Solved
-                    };
-                }
-                continue;
-            }
-
-            match self.solve_recursive(result, offset + 1) {
+            match self.solve_recursive_reverse(new_answer, offset - 1, use_combine_operator) {
                 Result::Solved => {
                     return if operator == 2 {
                         Result::SolvedWithCombinator
@@ -158,4 +129,29 @@ impl Solver {
 
         Result::NotSolved
     }
+}
+
+fn cut_number_from_end(ends_with: usize, nr: usize) -> Option<usize> {
+    let mut exp = 1u32;
+    let mut temp = ends_with;
+    loop {
+        if temp < 10 {
+            break;
+        }
+
+        temp = temp / 10;
+        exp += 1;
+    }
+
+    let power = usize::pow(10, exp);
+    if (nr % power) != ends_with {
+        return None;
+    }
+
+    let new_nr = (nr - ends_with) / power;
+    if new_nr == 0 {
+        return None;
+    }
+
+    Some(new_nr)
 }
