3e48c5954419d078b2334a02a8eda8918393b24f
mjarkk <mkopenga@gmail.com>
2024-12-24 00:12:50 +0100

    add comments to day 22

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -8,15 +8,15 @@ fn main() {
     let now = Instant::now();
     let puzzle = fs::read_to_string("./puzzle.txt").unwrap();
 
-    let mut global_price_tracker = [0u16; 262143];
+    // This doesn't feel great but is just 20x faster than a hashmap.
+    let mut global_price_tracker = [0u16; 262_143];
+    let mut monkey_price_tracker = [0u16; 262_143];
 
     let mut result = 0;
     for line in puzzle.lines() {
         if line.is_empty() {
             continue;
         }
-
-        let mut monkey_price_tracker = [0u16; 262143];
 
         let mut secret: usize = line.parse().unwrap();
         let mut prev_price = (secret % 10) as i8;
@@ -27,6 +27,9 @@ fn main() {
             let price = (secret % 10) as i8;
             let price_diff = price - prev_price;
 
+            // Track the changes to the price in 1 number.
+            // The first 4 digits are the absolute changes to the price.
+            // After the digets of a number that contains binary flags for each price change if it's a negative number.
             lookup_key = ((lookup_key * 10) + (price_diff.abs() as u32)) % 10000;
             lookup_key_negative_flags =
                 (lookup_key_negative_flags << 1) | if price_diff < 0 { 1 } else { 0 };
@@ -43,8 +46,9 @@ fn main() {
         }
         result += secret;
 
-        for (idx, value) in monkey_price_tracker.iter().enumerate() {
-            global_price_tracker[idx] += value;
+        for (idx, value) in monkey_price_tracker.iter_mut().enumerate() {
+            global_price_tracker[idx] += *value;
+            *value = 0;
         }
     }
     println!("p1: {}", result);
